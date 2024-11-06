f1d814d5c028452f6126c279b225eb4c56debaa3
Ilya Mzokov <Tayes134@yandex.ru>
2024-11-06 14:19:01 +0300

    Разиьраюсь с конфтест

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
 import time
-
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from test_function_and_variables import email_login, pass_login
+from function_and_variables import email_login, pass_login
 from locators import TestLocators
+
+
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
@@ -21,6 +21,6 @@ def login_correct(driver):
     WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable(TestLocators.BUTTON_ENTER_LOGIN))
     driver.find_element(*TestLocators.INPUT_EMAIL_NAME_PASS).send_keys(email_login)
     driver.find_element(*TestLocators.INPUT_PASS).send_keys(pass_login)
-    time.sleep(1)
+    WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable(TestLocators.BUTTON_ENTER_LOGIN))
     driver.find_element(*TestLocators.BUTTON_ENTER_LOGIN).click()
-    WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable(TestLocators.ELEMENENT_LOGIN_CHECK))
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located(TestLocators.ELEMENENT_LOGIN_CHECK))

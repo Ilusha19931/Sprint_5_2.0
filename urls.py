ab82887ce006d6366a8d28e57af2f7af934c642c
Ilya Mzokov <Tayes134@yandex.ru>
2024-11-08 14:19:30 +0300

    Проверил весь код изменил урл

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 
 class Urls:
-    URL_MAIN_PAGE = "https://stellarburgers.nomoreparties.site"
-    URL_ENTER_LOGIN = f"{URL_MAIN_PAGE}/login"
-    URL_REGISTRATION = f"{URL_MAIN_PAGE}/register"
-    URL_LK = f"{URL_MAIN_PAGE}/profile"
-    URL_RECOVER_PASS = f"{URL_MAIN_PAGE}/forgot-password"
+    URL_MAIN_PAGE = "https://stellarburgers.nomoreparties.site/"
+    URL_ENTER_LOGIN = f"{URL_MAIN_PAGE}login"
+    URL_REGISTRATION = f"{URL_MAIN_PAGE}register"
+    URL_LK = f"{URL_MAIN_PAGE}profile"
+    URL_RECOVER_PASS = f"{URL_MAIN_PAGE}forgot-password"
